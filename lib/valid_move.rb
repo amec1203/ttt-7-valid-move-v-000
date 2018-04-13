6ab3abfdb0cfdf2ff177181686ddaad72127e57a
Angela Campion <campion.angela@gmail.com>
2018-04-13 21:05:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, 0) && board[index].between?(0, 8)
+  if position_taken?(board, index) && board[index].between?(0, 8)
     true
   elsif
     false
